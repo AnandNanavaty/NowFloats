1fb27e3e90021d916d557d56edecd755689263b0
Anand <ananad.m@mantralabsglobal.com>
2020-01-22 23:43:43 +0530

    Initial Commit

---
+++ NowFloats/NowFloats.h
@@ -0,0 +1,19 @@
+//
+//  NowFloats.h
+//  NowFloats
+//
+//  Created by Anand Nanavaty on 22/01/20.
+//  Copyright © 2020 Anand Nanavaty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NowFloats.
+FOUNDATION_EXPORT double NowFloatsVersionNumber;
+
+//! Project version string for NowFloats.
+FOUNDATION_EXPORT const unsigned char NowFloatsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NowFloats/PublicHeader.h>
+
+
